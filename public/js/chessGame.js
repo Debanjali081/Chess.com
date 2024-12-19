5028ac69537ad2a048928e8f10655c195d04e505
Debanjali Lenka <debanjali017@gmail.com>
2024-12-19 18:45:29 +0530

    board rendering

--- public/js/chessGame.js
+++ public/js/chessGame.js
@@ -27,16 +27,40 @@ const renderBoard = () => {
                 pieceElement.innerText = "";
                 pieceElement.draggable = playerRole === square.color;
 
-                pieceElement.addEventListener('dragstart', () => {
+                pieceElement.addEventListener('dragstart', (e) => {
                     if (pieceElement.draggable) {
                         draggedPiece = pieceElement;
                         sourceSquare = { row: rowIndex, col: squareIndex };
+                        e.dataTransfer.setData("text/plan", "");
                     }
-                })
+                });
 
+                pieceElement.addEventListener("dragend",(e)=>{
+                    draggedPiece=null;
+                    sourceSquare=null;
+                });
+
+                squareElement.appendChild(pieceElement);
             }
-        })
+            squareElement.addEventListener("dragover",(e)=>{
+                e.preventDefault();
+            });
+            squareElement.addEventListener("drop",(e)=>{
+                e.preventDefault();
+                if(draggedPiece){
+                    const targetSource={
+                        row:parseInt(squareElement.dataset.row),
+                        col:parseInt(squareElement.dataset.col),
+                    };
+                    handleMove(sourceSquare,targetSource);
+                }
+            });
+            boardElement.appendChild(squareElement);
+        });
+        
     });
+
+   
 };
 
 
@@ -46,3 +70,4 @@ const handleMove = () => { };
 const getPieceUnicode = () => { };
 
 renderBoard();
+
